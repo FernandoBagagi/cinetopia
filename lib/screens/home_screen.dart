78ae18cb2f987801dc935527adc4c0cf2fb05aba
Fernando Bagagi <fernandosmartphone96@gmail.com>
2025-07-09 14:32:17 -0300

    cria o _fundoDegrade na HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _fundoDegrade(
+        corComecoDegrade: Theme.of(context).colorScheme.onSecondary,
+        corFimDegrade: Theme.of(context).colorScheme.surface,
+        body: _body(),
+      ),
+    );
+  }
+
+  Widget _fundoDegrade({
+    required Color corComecoDegrade,
+    required Color corFimDegrade,
+    required Widget body,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [corComecoDegrade, corComecoDegrade],
+        ),
+      ),
+      child: body,
+    );
+  }
+
+  Widget _body() {
+    return Placeholder();
   }
 }
