1ecdd18a1ae77ae93bd07b18efac76a25789a890
Fernando Bagagi <fernandosmartphone96@gmail.com>
2025-07-09 14:10:27 -0300

    cria cores.dart

---
+++ lib/widgets/cores.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const Color primaria       = Color(0xFFB370FF);
+const Color primariaHover  = Color(0xFFCEA1FF);
+const Color onPrimaria     = Color(0xFFEBEBEB);
+
+const Color secundaria     = Color(0xFF30D5C8); 
+const Color onSecundaria   = Color(0xFF151317);
+
+const Color fundoEscuro    = Color(0xFF151317);
+const Color surfaceDark    = Color(0xFF1D0E44);
+const Color onSurfaceDark  = Color(0xFFEBEBEB);
+
+const Color roxoEscuro     = Color(0xFF151317);
+const Color roxoMedio      = Color(0xFF6D63BC);
+const Color roxoClaro     = Color(0xFFD3BBE6);
+
+const Color cinzaNeutro    = Color(0xFFA5A5A5);
+const Color onGrayDark     = Color(0xFF151317);
+
+const Color accentWarm     = Color(0xFFBF5700);
+const Color onAccentWarm   = Color(0xFFEBEBEB);
